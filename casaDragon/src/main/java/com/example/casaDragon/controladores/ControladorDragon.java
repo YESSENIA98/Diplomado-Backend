da358f85d13fe0ee14cacb17e43e1c0d362d376f
Yessenia Martinez <113077413+YESSENIA98@users.noreply.github.com>
2024-07-16 15:05:46 -0500

    class: controller documentation

--- casaDragon/src/main/java/com/example/casaDragon/controladores/ControladorDragon.java
+++ casaDragon/src/main/java/com/example/casaDragon/controladores/ControladorDragon.java
@@ -1,7 +1,15 @@
 package com.example.casaDragon.controladores;
 
+import com.example.casaDragon.DTO.DragonDTO;
 import com.example.casaDragon.models.Dragon;
 import com.example.casaDragon.servicios.DragonServicio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("casadragonapi/v1/dragon")
+@Tag(name="Servicios Dragon", description = "Servicios asociados a la tabla Dragon de la bd dragrones, se hace CRUD completo")
 public class ControladorDragon {
 
     //el controlador necesita el servicio, se inyecta el servicio
@@ -16,6 +25,33 @@ public class ControladorDragon {
     DragonServicio dragonServicio;
 
     @PostMapping
+    @Operation(
+            summary = "Registra un dragon nuevo en la base de datos",
+            description = "Al llevar nombres, edad, altura se registra con éxito el dragon"
+    )
+    @ApiResponses(
+            value= {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Dragon creado con éxito en BD",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = DragonDTO.class),
+                                    examples = @ExampleObject(value = "{\"nombres\":\"Letti\", \"edad\":\"400\"}")
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Error al registrar el Dragon",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = String.class),
+                                    examples = @ExampleObject(value = "{\"error\":\"Datos invalidos\"}")
+                            )
+                    )
+            }
+    )
+
     //agregar un dragon
     public ResponseEntity<?> guardarDragon(@RequestBody Dragon datosClienteDragon) {
         try{
